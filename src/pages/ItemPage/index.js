9840f775490241e19157c8f8c2ff7a4a851ce30a
37flwr <nesterishinyarik@gmail.com>
2022-07-06 16:38:20 +0300

    Add item page content and stylings

--- src/pages/ItemPage/index.js
+++ src/pages/ItemPage/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { useParams } from 'react-router';
 import ApolloClient, { gql } from 'apollo-boost'
 import './styles.scss'
+import classNames from 'classnames';
 
 const withParams = (Component) => {
   return props => <Component {...props} params={useParams()} />;
@@ -47,13 +48,14 @@ class ItemPage extends Component {
           `
         })
         this.setState({
-            product: response.data.product
+            product: response.data.product,
+            mainImg: response.data.product.gallery[0]
         })
     }
 
   async componentDidMount() {
     this.setState({
-      type: this.props.params.id.slice(1)
+      type: this.props.params.id.slice(1),
     })
     await this.handleCategoryFetch(this.props.params.id.slice(1))
   }
@@ -70,7 +72,64 @@ class ItemPage extends Component {
   render() {
     console.log(this.state?.product);
     return (
-      <div>{this.state?.product?.id}</div>
+      <section className='item-page'>
+        <div className='item-photos-gallery'>
+          {this.state?.product?.gallery.map((img) => 
+            <img 
+              src={img}
+              alt=""
+              className='item-photo'
+              onClick={() => this.setState({
+                mainImg: img
+              })} 
+            />
+          )}
+        </div>
+        <div className='item-main-photo'>
+          <img src={this.state?.mainImg} alt="" />
+        </div>
+        <div className='item-details'>
+          <div className='item-vitals'>
+            <span className='item-brand'>
+              {this.state?.product?.brand}
+            </span>
+            <span className='item-title'>
+              {this.state?.product?.name}
+            </span>
+          </div>
+          <div className='item-attributes'>
+              {this.state?.product?.attributes.map((attr) => 
+                <div className='item-attribute'>
+                  <div className='item-attribute-heading'>
+                    {attr.id}:
+                  </div>
+                  <div className='item-attribute-list'>
+                    {attr.items.map((item) => 
+                      attr.type === 'text' ?
+                        <div className='item-text-attribute'>
+                          {item.value}
+                        </div>
+                      :
+                        <div className='item-color-attribute' style={{'backgroundColor': item.value}}/>
+                    )}
+                  </div>
+                </div>
+              )}
+              <div className='item-price'>
+                <span className='item-price-heading'>
+                  Price:
+                </span>
+                <span className='item-price-value'>
+                  12
+                </span>
+              </div>
+              <button className='add-to-cart-btn'>
+                Add to cart
+              </button>
+              <span dangerouslySetInnerHTML={{ __html: this.state?.product?.description }} />
+          </div>
+        </div>
+      </section>
     )
   }
 }
