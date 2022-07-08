ee8a4a5243729cc7e5fa6749170caaa64418e5b9
37flwr <nesterishinyarik@gmail.com>
2022-07-08 13:31:02 +0300

    Add price render to cart item

--- src/components/Layout/Header/components/Details/Cart/components/Item/ItemDetails/index.js
+++ src/components/Layout/Header/components/Details/Cart/components/Item/ItemDetails/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 import Attribute from './Attribute'
 import './styles.scss'
 
-export default class ItemDetails extends Component {
+class ItemDetails extends Component {
     render() {
-        console.log(this.props.item);
+        console.log(this.props.currency);
         return (
         <div className='item-details'>
             <div className='item-vitals'>
@@ -16,7 +17,11 @@ export default class ItemDetails extends Component {
                 </span>
             </div>
             <span className='item-price'>
-                12
+                {this.props.item.item.prices.map((curr) => {
+                    if (curr.currency.label.toLowerCase() === this.props.currency.currency) {
+                        return <>{curr.currency.symbol} {curr.amount}</>
+                    }
+                })}
             </span>
             <div className='item-attr-container'>
                 {this.props.item.item?.attributes?.map((attr) => 
@@ -27,3 +32,9 @@ export default class ItemDetails extends Component {
     )
   }
 }
+
+const mapStateToProps = state => ({
+    currency: state.Currency
+});
+
+export default connect(mapStateToProps, null)(ItemDetails)
