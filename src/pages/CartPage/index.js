e95e54d7562291ed365e8d70417e1654fd3c5c8b
37flwr <nesterishinyarik@gmail.com>
2022-07-10 01:37:06 +0300

    Add cart checkout section; add cart checkout section stylings

--- src/pages/CartPage/index.js
+++ src/pages/CartPage/index.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 
 import Item from './Item'
 
+import { handleCountCartItems, handleCountCartTotal } from '../../utils'
+
 import './styles.scss'
 
 class CartPage extends Component {
@@ -16,10 +18,39 @@ class CartPage extends Component {
                 <div className='cart-line' />
                 {this.props.cart.cart && this.props.cart.cart.length > 0 ? 
                     <>
-                    {this.props.cart.cart.map((item) => 
-                        <Item item={item} />
-                    )}
-                    <div className='cart-line' />
+                        {this.props.cart.cart.map((item) => 
+                            <Item item={item} />
+                        )}
+                        <div className='cart-line' />
+                        <div className='cart-checkout-part'>
+                            <div className='cart-checkout-line'>
+                                <span className='cart-chechout-title'>
+                                    Tax 21%:
+                                </span>
+                                <span className='cart-checkout-value'>
+                                    {handleCountCartTotal(this.props.cart.cart, this.props.currency).currSymbol}{(handleCountCartTotal(this.props.cart.cart, this.props.currency).total * 0.21).toFixed(2)}
+                                </span>
+                            </div>
+                            <div className='cart-checkout-line'>
+                                <span className='cart-chechout-title'>
+                                    Quantity:
+                                </span>
+                                <span className='cart-checkout-value'>
+                                    {handleCountCartItems(this.props.cart.cart)}
+                                </span>
+                            </div>
+                            <div className='cart-checkout-line'>
+                                <span className='cart-chechout-title'>
+                                    Total:
+                                </span>
+                                <span className='cart-checkout-value'>
+                                    {handleCountCartTotal(this.props.cart.cart, this.props.currency).currSymbol}{handleCountCartTotal(this.props.cart.cart, this.props.currency).total}
+                                </span>
+                            </div>
+                            <button className='cart-checkout-btn'>
+                                Order
+                            </button>
+                        </div>
                     </>
                 :
                     <h1>
@@ -32,7 +63,8 @@ class CartPage extends Component {
 }
 
 const mapStateToProps = state => ({
-    cart: state.Cart
+    cart: state.Cart,
+    currency: state.Currency
 });
 
 export default connect(mapStateToProps, null)(CartPage)
