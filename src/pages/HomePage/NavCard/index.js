fb7165a4dc1c7acf7c1d535347f2f818a4537ecd
37flwr <nesterishinyarik@gmail.com>
2022-07-16 16:19:42 +0300

    Refactor home page

--- src/pages/HomePage/NavCard/index.js
+++ src/pages/HomePage/NavCard/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { capitalizeFirstLetter } from '../../../../utils'
+import { capitalizeFirstLetter } from '../../../utils'
+
 import './styles.scss'
 
 export default class NavCard extends Component {
   render() {
     return (
         <Link
-            className='nav-card'
+            className='home-page-nav-card'
             to={`/:${this.props.category}`}
         >
           {capitalizeFirstLetter(this.props.category)}
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react'
-import ApolloClient, { gql } from 'apollo-boost'
-
-import NavCard from './components/NavCard';
+import NavCard from './NavCard';
 import Loader from '../../components/Loader';
+import { handleCategoriesFetch } from '../../utils';
 
 import './styles.scss'
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000'
-});
-
 export default class HomePage extends Component {
-  componentDidMount() {
-    this.handleCategoriesFetch()
+  state = {
+    categories: null
+  }
+
+  async componentDidMount() {
+    this.setState({
+      categories: await handleCategoriesFetch()
+    })
   }
 
   render() {
     return (
       <section id='home-page'>
-        {this.state?.categories ?
+        {this.state.categories ?
           <div className='home-page-grid'>
-            {this.state?.categories.map(({name}, idx) => 
+            {this.state.categories.map(({name}, idx) => 
               <NavCard key={idx} category={name} />
             )}
           </div>
